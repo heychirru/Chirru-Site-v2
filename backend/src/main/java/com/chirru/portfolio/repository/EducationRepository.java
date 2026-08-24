c59cb0bb43efd328d0133e91b7d415afc0107fba
Chiranjit Das <147528779+heychirru@users.noreply.github.com>
2026-08-24 09:47:26 +0530

    feat: add education repository

---
+++ backend/src/main/java/com/chirru/portfolio/repository/EducationRepository.java
@@ -0,0 +1,10 @@
+package com.chirru.portfolio.repository;
+
+import com.chirru.portfolio.entity.Education;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface EducationRepository extends JpaRepository<Education, Long> {
+    List<Education> findAllByOrderByDisplayOrderAscStartDateDesc();
+}
